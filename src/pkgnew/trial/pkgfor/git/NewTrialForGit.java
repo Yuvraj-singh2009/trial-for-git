22afb8760556654ea6230d7c6ce91468f3146771
User-Team01 <149222395+User-Team01@users.noreply.github.com>
2023-10-29 22:06:43 +0530

    Update NewTrialForGit.java

--- src/pkgnew/trial/pkgfor/git/NewTrialForGit.java
+++ src/pkgnew/trial/pkgfor/git/NewTrialForGit.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+// i changed and removed some code
 package pkgnew.trial.pkgfor.git;
 
-/**
- *
- * @author devis
- */
+
 public class NewTrialForGit {
 
     /**
